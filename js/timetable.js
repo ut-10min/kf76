750d092123ad779a1760580e518aff16d11399c4
matsumoto <mty.to314_Kyo.re@icloud.com>
2025-11-09 15:40:24 +0900

    タイムテーブル等を反映

--- js/timetable.js
+++ js/timetable.js
@@ -25,28 +25,18 @@ function construstTimeTable(timeTable, talksData) {
                 return { time: "\xa0", name: "\xa0", title: "", major: "" };
             }
 
-            // 休憩・座談会
-            else if (name.indexOf("休憩・座談会") == 0) {
-                return { time: time, name: "", title: name, major: "" };
+            // ---- 座談会（今年は「座談会」表記。念のため旧表記も許容）----
+            if (name.indexOf("座談会") === 0 || name.indexOf("休憩・座談会") === 0) {
+                return { time: time, name: "", title: "座談会", major: "" };
             }
 
             // 平野さんWS
-            else if (name == "平野WS") {
+            else if (name == "十河WS") {
                 return {
                     time: time,
-                    name: "ワークショップ：平野力也",
-                    title: "ワークショップ：骨の鑑定入門：これってヒト？イヌ？シカ？",
-                    major: "理学系研究科"
-                };
-            }
-
-            // 平城さんWS
-            else if (name == "平城WS") {
-                return {
-                    time: time,
-                    name: "ワークショップ：平城裕隆",
-                    title: "ワークショップ：騒がしくても話せる音声入力インタフェース",
-                    major: "学際情報学府学際情報学専攻"
+                    name: "ワークショップ：十河翔",
+                    title: "自分の「推し（好きなもの）」を言語化するとともに、他者の「推し」やその理由を知ることを通して、「推し」が社会や生活と、どのように紐づいているかを考える、その最初のキッカケを得ることとします。",
+                    major: "学際情報学府"
                 };
             }
             
@@ -59,29 +49,24 @@ function construstTimeTable(timeTable, talksData) {
                   major: talk.affiliation
                 };
               }
-            
-            // 通常講演者
-            /*else {
-                return {
-                    time: time,
-                    name: talk.name,
-                    title: talk.title,
-                    major: talk.affiliation
-                };
-            }    */
+
         });
 
 }
 
+
+// ▼ 3日分に拡張（ヘッダは 22/23/24 日）
 $(function () {
-    var DayTable1 = construstTimeTable(day1, data);
-    var DayTable2 = construstTimeTable(day2, data);
+  var DayTable1 = construstTimeTable(day1, data);
+  var DayTable2 = construstTimeTable(day2, data);
+  var DayTable3 = construstTimeTable(day3, data);
 
-    var template = $('#template').html();
-    Mustache.parse(template);
+  var template = $('#template').html();
+  Mustache.parse(template);
 
-    var rendered1 = Mustache.render(template, { table: DayTable1, header: "5/24 (土)" });
-    var rendered2 = Mustache.render(template, { table: DayTable2, header: "5/25 (日)" });
+  var rendered1 = Mustache.render(template, { table: DayTable1, header: "11/22（土）" });
+  var rendered2 = Mustache.render(template, { table: DayTable2, header: "11/23（日）" });
+  var rendered3 = Mustache.render(template, { table: DayTable3, header: "11/24（月）" });
 
-    $('.article-headline').html(rendered1 + "<br />" + rendered2);
+  $('.article-headline').html([rendered1, "<br />", rendered2, "<br />", rendered3].join(""));
 });
